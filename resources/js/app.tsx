6b184e043e01adbaac372ab9ecd5837d579b769e
Bilal Raja <rajabilal555@gmail.com>
2023-09-20 17:57:13 +0500

    added shadcn and ThemeProvider

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,19 +1,23 @@
 import './bootstrap';
 import '../css/app.css';
 
-import { createRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import {createRoot} from 'react-dom/client';
+import {createInertiaApp} from '@inertiajs/react';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
+import {ThemeProvider} from "@/lib/ThemeProvider";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
-    setup({ el, App, props }) {
+    setup({el, App, props}) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(
+            <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+                <App {...props} />
+            </ThemeProvider>);
     },
     progress: {
         color: '#4B5563',
