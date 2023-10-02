b5544f3914f10d6ff36ea80a47bac63fa597af15
Hammad-star <hammad.skn1@gmail.com>
2023-10-02 23:15:37 +0500

    Updated more ui stuff in staff dashboard

--- resources/js/Pages/Staff/Dashboard.tsx
+++ resources/js/Pages/Staff/Dashboard.tsx
@@ -14,32 +14,32 @@ export default function Dashboard({ auth }: PageProps) {
         >
             <Head title="Dashboard" />
 
-            <div className="py-12">
+            <div className="py-8">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="grid grid-rows-2-4  p-4 text-black ">
-                        <div className="grid grid-cols-4  gap-5">
-                            <section className="mb-4 p-4 bg-white h-28">
+                    <div className="grid grid-rows-2-4  p-4 text-black">
+                        <div className="grid grid-cols-4  gap-5 mb-5">
+                            <section className=" p-4 bg-white h-28 rounded-lg">
                                 <h2 className="text-l font-bold">
                                     Total Students
                                 </h2>
                                 <p className="text-2xl font-bold">400</p>
                                 <h3>some text like that</h3>
                             </section>
-                            <section className="mb-4 p-4 bg-white">
+                            <section className=" p-4 bg-white rounded-lg">
                                 <h2 className="text-l font-bold">
                                     Total Advisors
                                 </h2>
                                 <p className="text-2xl font-bold">15</p>
                                 <h3>some text like that</h3>
                             </section>
-                            <section className="mb-4 p-4 bg-white">
+                            <section className=" p-4 bg-white rounded-lg">
                                 <h2 className="text-l font-bold">
                                     Slots available
                                 </h2>
                                 <p className="text-2xl font-bold">24</p>
                                 <h3>some text like that</h3>
                             </section>
-                            <section className="mb-4 p-4 bg-white">
+                            <section className=" p-4 bg-white rounded-lg">
                                 <h2 className="text-l font-bold">
                                     Total Student Groups
                                 </h2>
@@ -47,48 +47,217 @@ export default function Dashboard({ auth }: PageProps) {
                                 <h3>some text like that</h3>
                             </section>
                         </div>
-                        <div className="grid grid-cols-2  gap-8">
-                            <section className="mb-4 p-4 ml-8 bg-white h-96">
+                        <div className="grid grid-cols-2  gap-4 items-center">
+                            <section className="p-4 ml-12  bg-white rounded-lg h-auto">
                                 <h2 className="text-xl font-bold">
                                     Open Advisors
                                 </h2>
-                                <p className="">Ads with posts</p>
-                                <div className="flex justify-between gap-1">
-                                    <div className="flex gap-3">
-                                        <div className="tag">FF</div>
-                                        <div>Olivi Martin</div>
+                                <p className="mb-6">Ads with posts</p>
+                                <div className="flex justify-between gap-1 mb-10 ">
+                                    <div className="flex gap-3 ">
+                                        <div className=" bg-gray-100 w-12 h-12 rounded-full inline-flex items-center justify-center ">
+                                            FF
+                                        </div>
+                                        <div className="inline-flex items-center">
+                                            Olivi Martin
+                                        </div>
                                     </div>
                                     <div>
-                                        <div className="">2 Slots</div>
+                                        <div className="inline-flex items-center">
+                                            2 Slots
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="flex justify-between gap-1 mb-10">
+                                    <div className="flex gap-3 ">
+                                        <div className=" bg-gray-100 w-12 h-12 rounded-full inline-flex items-center justify-center ">
+                                            FF
+                                        </div>
+                                        <div className="inline-flex items-center">
+                                            Jackson Lee
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="inline-flex items-center">
+                                            2 Slots
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="flex justify-between gap-1 mb-10">
+                                    <div className="flex gap-3 ">
+                                        <div className=" bg-gray-100 w-12 h-12 rounded-full inline-flex items-center justify-center ">
+                                            FF
+                                        </div>
+                                        <div className="inline-flex items-center">
+                                            Olivi Martin
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="inline-flex items-center">
+                                            2 Slots
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="flex justify-between gap-1 mb-10 ">
+                                    <div className="flex gap-3 ">
+                                        <div className=" bg-gray-100 w-12 h-12 rounded-full inline-flex items-center justify-center ">
+                                            FF
+                                        </div>
+                                        <div className="inline-flex items-center">
+                                            Olivi Martin
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="inline-flex items-center">
+                                            2 Slots
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="flex justify-between gap-1 mb-10 ">
+                                    <div className="flex gap-3 ">
+                                        <div className=" bg-gray-100 w-12 h-12 rounded-full inline-flex items-center justify-center ">
+                                            FF
+                                        </div>
+                                        <div className="inline-flex items-center">
+                                            Olivi Martin
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="inline-flex items-center">
+                                            2 Slots
+                                        </div>
                                     </div>
                                 </div>
                             </section>
-                            <section className="mb-4 p-4 mr-8 bg-white ">
+                            <section className="p-4 mr-32 mt-5 bg-white rounded-lg h-auto">
                                 <h2 className="text-xl font-bold">
                                     Recent Activity
                                 </h2>
-                                <ul>
-                                    <li>Olivia Martin</li>
-                                    <li>Jackson Las</li>
-                                    <li>Isabella Nguyen</li>
-                                    <li>Wiliam Kim</li>
-                                    <li>Sofia Davis</li>
+                                <p>Activity of all the projects</p>
+                                <ul className="mt-6">
+                                    <li>
+                                        <div className="flex  gap-1 mb-10 ">
+                                            <div className="flex gap-3">
+                                                <div className=" bg-gray-100 w-12 h-12 rounded-full inline-flex items-center justify-center ">
+                                                    FF
+                                                </div>
+                                                <div className="flex flex-col">
+                                                    <div className="font-bold">
+                                                        FYP Forge | Olivia
+                                                        Martin
+                                                    </div>
+                                                    <p className="">
+                                                        5 mins ago - updated
+                                                        project details
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="flex  gap-1 mb-10 ">
+                                            <div className="flex gap-3">
+                                                <div className=" bg-gray-100 w-12 h-12 rounded-full inline-flex items-center justify-center ">
+                                                    FF
+                                                </div>
+                                                <div className="flex flex-col">
+                                                    <div className="font-bold">
+                                                        FYP Forge | Jackson Las
+                                                        Martin
+                                                    </div>
+                                                    <p className="">
+                                                        1 hr ago - Submitted for
+                                                        approval
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="flex  gap-1 mb-10 ">
+                                            <div className="flex gap-3">
+                                                <div className=" bg-gray-100 w-12 h-12 rounded-full inline-flex items-center justify-center ">
+                                                    FF
+                                                </div>
+                                                <div className="flex flex-col">
+                                                    <div className="font-bold">
+                                                        SZABDESK | Isabella
+                                                        Nguyen Martin
+                                                    </div>
+                                                    <p className="">
+                                                        2 hrs ago - Marked by
+                                                        panel
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="flex  gap-1 mb-10 ">
+                                            <div className="flex gap-3">
+                                                <div className=" bg-gray-100 w-12 h-12 rounded-full inline-flex items-center justify-center ">
+                                                    FF
+                                                </div>
+                                                <div className="flex flex-col">
+                                                    <div className="font-bold">
+                                                        FYP Forge | Willian Kim
+                                                        Martin
+                                                    </div>
+                                                    <p className="">
+                                                        6 hrs ago - updated
+                                                        project details
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="flex  gap-1 mb-10 ">
+                                            <div className="flex gap-3">
+                                                <div className=" bg-gray-100 w-12 h-12 rounded-full inline-flex items-center justify-center ">
+                                                    FF
+                                                </div>
+                                                <div className="flex flex-col">
+                                                    <div className="font-bold">
+                                                        FYP Forge | Sofia Davis
+                                                        Martin
+                                                    </div>
+                                                    <p className="">
+                                                        5 mins ago - updated
+                                                        project details
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
                                 </ul>
+                                <a
+                                    href="#"
+                                    className="flex justify-center gap-2"
+                                >
+                                    <div className="inline-flex font-bold align-text-top text-lg ">
+                                        View all activities
+                                    </div>
+                                    <div className="inline-flex items-center w-4">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            strokeWidth={1.5}
+                                            stroke="currentColor"
+                                            className="w-6 h-6"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                                            />
+                                        </svg>
+                                    </div>
+                                </a>
                             </section>
                         </div>
                     </div>
-                    {/* <div className="grid grid-cols-2">
-                        <div className="grid grid-cols-2 h-80 w-110 mt-10 ">
-                            <div className="shadow-md"></div>
-                            <div className="shadow-md"></div>
-                            <div className="shadow-md"></div>
-                            <div className="shadow-md"></div>
-                        </div>
-                        <div className="grid grid-cols-2 h-80 w-160 mt-10 ">
-                            <div className="shadow-md"></div>
-                            <div className="shadow-md"></div>
-                        </div>
-                    </div> */}
                 </div>
             </div>
         </AuthenticatedLayout>
