b0ff0285fc48fc1f55ef3d36f183701b7fdeb484
Bilal Raja <rajabilal555@gmail.com>
2023-11-04 21:14:31 +0500

    updated test cases

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature\Auth;
 
+use App\Models\Advisor;
+use App\Models\Student;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -18,17 +20,46 @@ class AuthenticationTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function test_users_can_authenticate_using_the_login_screen(): void
+    public function test_staff_can_authenticate_using_the_login_screen(): void
     {
         $user = User::factory()->create();
 
         $response = $this->post('/login', [
             'email' => $user->email,
             'password' => 'password',
+            'type' => 'staff',
         ]);
 
         $this->assertAuthenticated();
-        $response->assertRedirect(RouteServiceProvider::HOME);
+        $response->assertRedirect(route('staff.dashboard'));
+    }
+
+    public function test_advisor_can_authenticate_using_the_login_screen(): void
+    {
+        $user = Advisor::factory()->create();
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password',
+            'type' => 'advisor',
+        ]);
+
+        $this->assertAuthenticated('advisor');
+        $response->assertRedirect(route('advisor.dashboard'));
+    }
+
+    public function test_student_can_authenticate_using_the_login_screen(): void
+    {
+        $user = Student::factory()->create();
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password',
+            'type' => 'student',
+        ]);
+
+        $this->assertAuthenticated('student');
+        $response->assertRedirect(route('student.dashboard'));
     }
 
     public function test_users_can_not_authenticate_with_invalid_password(): void
@@ -38,6 +69,7 @@ class AuthenticationTest extends TestCase
         $this->post('/login', [
             'email' => $user->email,
             'password' => 'wrong-password',
+            'type' => 'staff',
         ]);
 
         $this->assertGuest();
