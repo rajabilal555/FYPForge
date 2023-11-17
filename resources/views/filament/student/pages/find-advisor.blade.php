b5139028224af86a2fb474bcc3538802546f3091
Hammad-star <hammad.skn1@gmail.com>
2023-11-17 11:52:04 +0500

    Added Advisor MyProjects page

--- resources/views/filament/student/pages/find-advisor.blade.php
+++ resources/views/filament/student/pages/find-advisor.blade.php
@@ -1,13 +1,3 @@
 <x-filament-panels::page>
-    @php
-    $normalizeWidgetClass = function (string | Filament\Widgets\WidgetConfiguration $widget): string {
-    if ($widget instanceof \Filament\Widgets\WidgetConfiguration) {
-    return $widget->widget;
-    }
-
-    return $widget;
-    };
-    @endphp
-
-    @livewire($normalizeWidgetClass(\App\Filament\Student\Widgets\AdvisorList::class))
+    {{$this->table}}
 </x-filament-panels::page>
