9b6eda388ba2cd969f686761945954344ca667ab
Bilal Raja <rajabilal555@gmail.com>
2023-11-19 23:36:05 +0500

    add test routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/debug', function () {
+    // Print out all debug information relative to the proxy request.
+    return response()->json([
+        'request_ip' => request()->ip(),
+        'headers' => request()->headers->all(),
+        'server' => request()->server->all(),
+        'trusted_proxies' => request()->getTrustedProxies(),
+        'is_trusted_proxy' => request()->isFromTrustedProxy(),
+        'is_secure' => request()->secure(),
+        'test url' => url('/'),
+        'test asset' => asset('/'),
+        'test signed Url' => URL::temporarySignedRoute(
+            'test_signed_url',
+            now()->addMinutes(30)
+        ),
+    ]);
+});
+
+Route::get('/test_signed_url', function () {
+    return response()->json([
+        'data' => request()->input(),
+        'request_ip' => request()->ip(),
+        'headers' => request()->headers->all(),
+        'server' => request()->server->all(),
+        'trusted_proxies' => request()->getTrustedProxies(),
+        'is_trusted_proxy' => request()->isFromTrustedProxy(),
+        'is_signature_valid' => request()->hasValidSignature(),
+        'is_secure' => request()->secure(),
+    ]);
+})->name('test_signed_url');
